0e66eee176b20b6676b507296c8cdee2a7461431
Dima-Kalmykov <dekalmykov@edu.hse.ru>
2020-01-25 23:39:44 +0300

    Task_2 is done

--- 03_module/02_seminar/class_work/Task_2/Task_2/Car.cs
+++ 03_module/02_seminar/class_work/Task_2/Task_2/Car.cs
@@ -1,42 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_2
 {
+    // Declare delegate.
     public delegate void CarEngineHandler(string msgForCaller);
 
+    /// <summary>
+    /// Class car.
+    /// </summary>
     class Car
     {
         private CarEngineHandler listOfHandlers;
-        internal int CurrentSpeed { get; set; }
-        internal int MaxSpeed { get; set; }
-        internal string PetName { get; set; }
 
+        // Info about car.
+        internal int CurrentSpeed { get; private set; }
+        internal int MaxSpeed { get; }
+        internal string PetName { get; }
+
+        // Car's condition.
         private bool carIsDead;
 
+        // Default constructor.
         public Car() =>
             MaxSpeed = 100;
 
-        public Car(string name, int maxSp, int currSp) =>
-            (PetName, MaxSpeed, CurrentSpeed) = (name, maxSp, currSp);
+        // Constructor.
+        public Car(string name, int maxSp, int curSp) =>
+            (PetName, MaxSpeed, CurrentSpeed) = (name, maxSp, curSp);
 
+        // Set method for notifications.
         public void RegisterWithCarEngine(CarEngineHandler methodToCall) =>
             listOfHandlers = methodToCall;
 
+        /// <summary>
+        /// Method for accelerate speed.
+        /// </summary>
+        /// <param name="delta"> Figure for change speed </param>
         public void Accelerate(int delta)
         {
             if (carIsDead)
                 listOfHandlers?.Invoke("К сожалению, машина сломана :( ...");
             else
             {
+                // Increase speed.
                 CurrentSpeed += delta;
 
+                // First notification.
                 if (10 == MaxSpeed - CurrentSpeed)
                     listOfHandlers?.Invoke("Предупреждение! Будь осторожнее");
 
+                // Last notification.
                 if (CurrentSpeed >= MaxSpeed)
                     carIsDead = true;
                 else
--- 03_module/02_seminar/class_work/Task_2/Task_2/Program.cs
+++ 03_module/02_seminar/class_work/Task_2/Task_2/Program.cs
@@ -1,34 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_2
 {
     class Program
     {
+        /// <summary>
+        /// Method for print notification.
+        /// </summary>
+        /// <param name="message"> Notification </param>
         public static void OnCarEngineEvent(string message)
         {
-            Console.WriteLine("\n***** Сообщение от объекта типа Car *****");
-            Console.WriteLine("=> {0}", message);
-            Console.WriteLine("******************************\n");
+            PrintMessage("\n***** Сообщение от объекта типа Car *****\n");
+            PrintMessage($"=> {message}\n", ConsoleColor.Yellow);
+        }
+
+        /// <summary>
+        /// Print color message.
+        /// </summary>
+        /// <param name="message"> Message </param>
+        /// <param name="color"> Message's color </param>
+        private static void PrintMessage(string message, ConsoleColor color = ConsoleColor.Cyan)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(message);
+            Console.ResetColor();
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("***** Использование делегатов для управления событиями *****\n");
-            
-            Car c1 = new Car("SlugBug", 100, 10);
+            // Print caption.
+            PrintMessage("***** Использование делегатов для управления событиями *****\n",
+                ConsoleColor.Yellow);
 
+            // Create new car.
+            var c1 = new Car("SlugBug", 100, 10);
+
+            // Method for notifications.
             c1.RegisterWithCarEngine(OnCarEngineEvent);
-
+            
+            // Increase speed.
             Console.WriteLine("***** Увеличиваем скорость *****");
             for (int i = 0; i < 6; i++)
                 c1.Accelerate(20);
 
-            Console.ReadLine();
+            PrintMessage("\nPress ESC for exit", ConsoleColor.Green);
+            while (Console.ReadKey().Key != ConsoleKey.Escape) ;
         }
     }
 }
