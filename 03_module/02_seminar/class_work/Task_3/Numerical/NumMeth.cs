4505b2ac39a13a456e80367f4e4faaac72c535bc
Dima-Kalmykov <dekalmykov@edu.hse.ru>
2020-01-26 00:17:57 +0300

    Task_3 is done

--- 03_module/02_seminar/class_work/Task_3/Numerical/NumMeth.cs
+++ 03_module/02_seminar/class_work/Task_3/Numerical/NumMeth.cs
@@ -1,92 +1,101 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Numerical
 {
+    // Declare delegat.
     public delegate double function(double x);
 
+    // Declare delegat.
     public delegate double Functional_1(double x);
 
+    /// <summary>
+    /// Class for analysis function.
+    /// </summary>
     public class NumMeth
     {
+        /// <summary>
+        /// Find minimum's point.
+        /// </summary>
+        /// <param name="fun"> Function </param>
+        /// <param name="left"> Left bound </param>
+        /// <param name="right"> Right bound </param>
+        /// <param name="delta"> Eps X </param>
+        /// <param name="epsilon"> Eps Y </param>
+        /// <returns> Minimum's point </returns>
         public static double Optimum_1(Functional_1 fun, double left, double right,
                double delta, double epsilon)
         {
+            // Declare require variable.
             double rOne = (Math.Sqrt(5) - 1) / 2.0;
             double rTwo = rOne * rOne;
             double yLeft, yRight;
-            double c, d;
             double h;
-            double p, yC, yD, yP;
 
-            yLeft = fun(left);
-            yRight = fun(right);
-            h = right - left;
-            c = left + rTwo * h;
-            yC = fun(c);
-            d = left + rOne * h;
-            yD = fun(d);
+            // Можно было делать обычное присваивание, но так лаконичней.
+            // Set value in variables.
+            (yLeft, yRight, h) = (fun(left), fun(right), right - left);
 
-            while (Math.Abs(yLeft-yRight)>epsilon || h>delta)
+            var c = left + rTwo * h;
+            var yC = fun(c);
+
+            var d = left + rOne * h;
+            var yD = fun(d);
+
+            // Function start.
+            while (Math.Abs(yLeft - yRight) > epsilon || h > delta)
             {
                 if (yC < yD)
                 {
-                    right = d;
-                    yRight = yD;
-                    d = c;
-                    yD = yC;
-                    h = right - left;
+                    (right, yRight) = (d, yD);
+                    (d, yD, h) = (c, yC, right - left);
+
                     c = left + rTwo * h;
                     yC = fun(c);
                 }
                 else
                 {
-                    left = c;
-                    yLeft = yC;
-                    c = d;
-                    yC = yD;
+                    (left, yLeft, c, yC) = (c, yC, d, yD);
+
                     h = right - left;
                     d = left + rOne * h;
                     yD = fun(d);
                 }
             }
 
-            p = left;
-            yP = yLeft;
-
-            if (yRight < yLeft)
-            {
-                p = right;
-                yP = yRight;
-            }
-
-            return p;
+            return (yRight < yLeft) ? right : left;
         }
 
+        /// <summary>
+        /// Find the root using the bisection method.
+        /// </summary>
+        /// <param name="left"> Left bound </param>
+        /// <param name="right"> Right bound </param>
+        /// <param name="epsX"> Eps X</param>
+        /// <param name="epsY"> Eps Y</param>
+        /// <param name="f"> Function </param>
+        /// <returns> Root of function on the interval </returns>
         public static double Bisec(double left, double right,
             double epsX, double epsY, function f)
         {
-            double x, y, z;
+            var x = left;
+            var y = f(x);
 
-            x = left;
-            y = f(x);
-
+            // Immediately check epsY.
             if (Math.Abs(y) <= epsY)
                 return x;
 
             x = right;
-            z = f(x);
+            var z = f(x);
 
+            // Again check EpsY.
             if (Math.Abs(z) <= epsY)
                 return x;
 
+            // Invalid range bounds.
             if (y * z > 0)
                 throw new IndexOutOfRangeException("Интервал не локализует корень функции");
 
+            // Bisec method.
             do
             {
                 x = left / 2 + right / 2;
--- 03_module/02_seminar/class_work/Task_3/Task_3/Program.cs
+++ 03_module/02_seminar/class_work/Task_3/Task_3/Program.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using Numerical;
+﻿using Numerical;
+using System;
 
 namespace Task_3
 {
     class Program
     {
-        #region consts for bisection method
-        private const double epsX = 0.01;
-        private const double epsY = 0;
-        private const double left = 3;
-        private const double right = 4;
+        #region Consts for bisection method.
+        private const double EpsX = 0.01;
+        private const double EpsY = 0;
+        private const double Left = 3;
+        private const double Right = 4;
         #endregion
 
-        #region consts for optimum method
-        private const double epsilon = 0.01; //y
-        private const double delta = 0.001;//x
+        #region Consts for optimum method.
+        private const double Epsilon = 0.01;
+        private const double Delta = 0.001;
         #endregion
 
+        /// <summary>
+        /// Print color message.
+        /// </summary>
+        /// <param name="message"> Message </param>
+        /// <param name="color"> Message's color </param>
         private static void PrintMessage(string message, ConsoleColor color = ConsoleColor.Cyan)
         {
             Console.ForegroundColor = color;
@@ -29,16 +29,25 @@ namespace Task_3
             Console.ResetColor();
         }
 
+        /// <summary>
+        /// Get Sin(x).
+        /// </summary>
+        /// <param name="x"> X </param>
+        /// <returns> Sin(x)</returns>
         private static double SinX(double x) => Math.Sin(x);
 
+        /// <summary>
+        /// Test bisection method.
+        /// </summary>
         private static void TestBisec()
         {
-            PrintMessage("Тест метода Bisec:\n\n", ConsoleColor.Green);
+            PrintMessage("Test Bisec method:\n\n", ConsoleColor.Green);
 
-            Console.WriteLine("Найдём корень уравнения Sin(x) = 0," +
-                              $" на отрезке [{left}, {right}]:" + Environment.NewLine);
+            Console.WriteLine("Find root of equation Sin(x) = 0," +
+                              $" on the segment [{Left}, {Right}]:" + Environment.NewLine);
 
-            function[] funcs =
+            // Types of functions.
+            function[] funcsArr =
             {
                 Math.Sin,
                 SinX,
@@ -46,44 +55,50 @@ namespace Task_3
                 x => Math.Sin(x)
             };
 
+            // Types of messages.
             string[] messages =
             {
-                "Поиск корня с использованием библиотечной функции:",
-                "Поиск корня с использованием статического метода, явно определённого в программе: ",
-                "Поиск корня с использованием анонимного метода: ",
-                "Поиск корня с использованием лямбда-выражения: "
+                "Finding a root using a library function: ",
+                "Finding a root using a static method explicitly defined in the program: ",
+                "Finding a root using an anonymous method: ",
+                "Finding a root using a lambda expression: "
             };
 
+            // Test.
             for (int i = 0; i < 4; i++)
             {
                 PrintMessage(messages[i]);
-                PrintMessage($"x = {NumMeth.Bisec(left, right, epsX, epsY, funcs[i])}\n",
+                PrintMessage($"x = {NumMeth.Bisec(Left, Right, EpsX, EpsY, funcsArr[i])}\n",
                     ConsoleColor.Yellow);
             }
 
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// Test optimum method.
+        /// </summary>
         private static void TestOptimum()
         {
-            PrintMessage("Тест метода Optimum_1:\n\n", ConsoleColor.Green);
+            PrintMessage("Test Optimum_1 method:\n\n", ConsoleColor.Green);
 
-            double minX;
-
-            string[] funcsText =
+            // Text presentation of functions.
+            string[] funcsTextArr =
             {
                 "Cos(x)",
                 "x * (x * x - 2) - 5",
                 "-Sin(x) - Sin(3 * x) / 3"
             };
 
-            Functional_1[] funcs =
+            // Types of functions.
+            Functional_1[] funcsArr =
             {
                 x => Math.Cos(x),
                 x => x * (x * x - 2) - 5,
                 x => -Math.Sin(x) - Math.Sin(3 * x) / 3
             };
 
+            // Bounds for every function.
             (int left, int right)[] bounds =
             {
                 (3, 6),
@@ -91,18 +106,20 @@ namespace Task_3
                 (0, 1)
             };
 
+            // Test method.
             for (int i = 0; i < 3; i++)
             {
-                minX = NumMeth.Optimum_1(funcs[i], bounds[i].left, bounds[i].right, delta, epsilon);
+                var minX = NumMeth.Optimum_1(funcsArr[i], bounds[i].left, bounds[i].right, Delta, Epsilon);
 
-                PrintMessage($"Точка минимума функции {funcsText[i]} " +
-                             $"на отрезке [{bounds[i].left}, {bounds[i].right}]: ");
+                PrintMessage($"Function minimum point {funcsTextArr[i]} " +
+                             $"on the segment [{bounds[i].left}, {bounds[i].right}]: ");
                 PrintMessage($"x = {minX}\n", ConsoleColor.Yellow);
             }
         }
 
         static void Main(string[] args)
         {
+            // Attempt to apply bisection method.
             try
             {
                 TestBisec();
@@ -113,6 +130,9 @@ namespace Task_3
             }
 
             TestOptimum();
+
+            PrintMessage("\nPress ESC for exit", ConsoleColor.Green);
+            while (Console.ReadKey().Key != ConsoleKey.Escape) ;
         }
     }
 }
