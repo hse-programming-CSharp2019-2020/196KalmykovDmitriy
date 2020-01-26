213da3f0ddaaba603660e3f0b0fb6ae92b33b048
Dima-Kalmykov <dekalmykov@edu.hse.ru>
2020-01-26 12:23:47 +0300

    Left to do the test

---
+++ 03_module/02_seminar/home_work/Independent work/Independent work/Comedian.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Independent_work
+{
+    /// <summary>
+    /// Class comedian.
+    /// </summary>
+    internal class Comedian
+    {
+        // Name.
+        private readonly string _name;
+
+        // Как альтернатива, проверку имени можно было вынести в отдельный метод,
+        // в котором бы проходили циклом по всему имени и проверяли каждый символ.
+        // Constructor.
+        internal Comedian(string name)
+        {
+            if (name is null || name.ToCharArray().Any(char.IsDigit))
+                throw new Exception("Name must not contain digits");
+
+            _name = name;
+        }
+
+        /// <summary>
+        /// Print info about comedian.
+        /// </summary>
+        internal void Identify() =>
+            Program.PrintMessage($"Я комик, меня зовут {_name}.\n", ConsoleColor.Yellow);
+
+        /// <summary>
+        /// Print joke.
+        /// </summary>
+        internal static void Joke() =>
+            Program.PrintMessage("Я не слечу с бюджета");
+    }
+}
---
+++ 03_module/02_seminar/home_work/Independent work/Independent work/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Independent_work
+{
+    internal class Program
+    {
+        // Declare delegate.
+        private delegate void JokerDelegate();
+
+        /// <summary>
+        /// Print color message.
+        /// </summary>
+        /// <param name="message"> Message </param>
+        /// <param name="color"> Message's color </param>
+        public static void PrintMessage(string message, ConsoleColor color = ConsoleColor.Cyan)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(message);
+            Console.ResetColor();
+        }
+
+        private static void Main()
+        {
+            Comedian comedian = default;
+
+            do
+            {
+                Console.Clear();
+
+                // Get name and create comedian with that name.
+                for (var i = 0; i < 1; i++)
+                {
+                    PrintMessage("Enter name of comedian without digits: ");
+                    try
+                    {
+                        comedian = new Comedian(Console.ReadLine());
+                    }
+                    catch (Exception ex)
+                    {
+                        PrintMessage(ex.Message + Environment.NewLine, ConsoleColor.Red);
+                        i--;
+                    }
+                }
+
+                // Check and method call.
+                if (comedian != null)
+                {
+                    JokerDelegate joker = comedian.Identify;
+                    joker += Comedian.Joke;
+
+                    joker();
+                }
+
+                // Message on further actions.
+                PrintMessage("\n\nPress ESC for exit, " +
+                             "press any other key for repeat solution", ConsoleColor.Green);
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
